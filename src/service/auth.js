e29c52a5e53ca4faff30133658121e8f070342f0
Pallav Jain <pallavjain12@gmail.com>
2023-04-29 16:21:39 +0530

    Edit check

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,6 +1,6 @@
 
 export const initUrl = "https://e885-119-161-98-68.ngrok-free.app";
-
+// I am able to edit this file
 export function isDoctor(){
     if(localStorage.getItem('id') && localStorage.getItem('type')==="doctor"){
         return true;
